87c1aaa4cd365119bc73753f83e8f41d33c11f88
Rodrigo Castillo <rodrigocastillo421@gmail.com>
2025-03-25 21:18:37 -0600

    add

---
+++ app/build.gradle.kts
@@ -0,0 +1,58 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+}
+
+android {
+    namespace = "com.example.rostros"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.example.rostros"
+        minSdk = 29
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+}
+
+dependencies {
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+
+    //Agregando las dependencias de MLkit, asi como la version de la camara
+    implementation ("com.google.mlkit:face-detection:16.1.5")
+    implementation ("com.google.android.gms:play-services-mlkit-face-detection:16.1.5")
+    implementation ("androidx.camera:camera-core:1.3.0")
+
+    val version = "1.4.1"
+    implementation("androidx.camera:camera-core:${version}")
+    implementation("androidx.camera:camera-camera2:${version}")
+    implementation("androidx.camera:camera-lifecycle:${version}")
+    implementation("androidx.camera:camera-view:${version}")
+}
---
+++ app/src/main/java/com/example/rostros/MainActivity.kt
@@ -0,0 +1,131 @@
+package com.example.rostros
+import android.Manifest
+import android.content.pm.PackageManager
+import android.graphics.Color
+import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.OptIn
+import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ExperimentalGetImage
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
+import androidx.core.content.ContextCompat
+import com.google.mlkit.vision.common.InputImage
+import com.google.mlkit.vision.face.FaceDetection
+import com.google.mlkit.vision.face.FaceDetector
+import com.google.mlkit.vision.face.FaceDetectorOptions
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var previewView: PreviewView
+    private lateinit var textView: TextView
+    private lateinit var faceDetector: FaceDetector
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        previewView = findViewById(R.id.previewView)
+        textView = findViewById(R.id.textView)
+
+        // Configurar el detector de rostros
+        val options = FaceDetectorOptions.Builder()
+            .setPerformanceMode(FaceDetectorOptions.PERFORMANCE_MODE_FAST)
+            .setLandmarkMode(FaceDetectorOptions.LANDMARK_MODE_NONE)
+            .setClassificationMode(FaceDetectorOptions.CLASSIFICATION_MODE_NONE)
+            .build()
+
+        faceDetector = FaceDetection.getClient(options)
+
+        // Solicitar permisos de la cámara
+        if (allPermissionsGranted()) {
+            Toast.makeText(this, "PERMISOS DE CAMARA CONCEDIDOS", Toast.LENGTH_LONG).show()
+            startCamera()
+        } else {
+            requestPermissions.launch(arrayOf(Manifest.permission.CAMERA))
+        }
+    }
+
+    @OptIn(ExperimentalGetImage::class)
+    private fun startCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+
+        cameraProviderFuture.addListener({
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+
+            val preview = Preview.Builder().build().also {
+                it.setSurfaceProvider(previewView.surfaceProvider)
+            }
+
+            val imageAnalysis = ImageAnalysis.Builder()
+                .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+                .build()
+                .also {
+                    it.setAnalyzer(ContextCompat.getMainExecutor(this)) { imageProxy ->
+                        val mediaImage = imageProxy.image
+                        if (mediaImage != null) {
+                            val image = InputImage.fromMediaImage(mediaImage, imageProxy.imageInfo.rotationDegrees)
+                            faceDetector.process(image)
+                                .addOnSuccessListener { faces ->
+                                    if (faces.isNotEmpty()) {
+                                        runOnUiThread {
+                                            textView.text = "Rostro detectado!"
+                                            textView.setBackgroundColor(Color.parseColor("#00913F"))
+                                        }
+                                    } else {
+                                        runOnUiThread {
+                                            textView.text = "No se ha detectado un rostro"
+                                            textView.setBackgroundColor(Color.parseColor("#CDCDCD"))
+                                        }
+                                    }
+                                }
+                                .addOnFailureListener { e ->
+                                    Log.e("MainActivity", "Error en la detección de rostros", e)
+                                }
+                                .addOnCompleteListener {
+                                    imageProxy.close()
+                                }
+                        }
+                    }
+                }
+
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
+            try {
+                cameraProvider.unbindAll()
+                cameraProvider.bindToLifecycle(
+                    this, cameraSelector, preview, imageAnalysis)
+            } catch(exc: Exception) {
+                Log.e("MainActivity", "Use case binding failed", exc)
+            }
+
+        }, ContextCompat.getMainExecutor(this))
+    }
+
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(baseContext, it) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private val requestPermissions = registerForActivityResult(
+        ActivityResultContracts.RequestMultiplePermissions()
+    ) { permissions ->
+        if (permissions[Manifest.permission.CAMERA] == true) {
+            startCamera()
+        } else {
+            Toast.makeText(this, "Por favor, acepte los permisos de camara", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    companion object {
+        private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
+    }
+
+}
